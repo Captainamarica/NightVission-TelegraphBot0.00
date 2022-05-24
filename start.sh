ba960aa66e9d56e28c2b55a0157ed010dff75f93
NAVANJANA <104069319+Captainamarica@users.noreply.github.com>
2022-05-24 18:56:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Captainamarica/NightVission-TelegraphBot0.00.git /Image-Upload-Bot
+git clone https://github.com/Captainamarica/NightVission-TelegraphBot0.00.git/Image-Upload-Bot
 cd /Image-Upload-Bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
